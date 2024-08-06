428beac3075aaaedad6904d53a25bd88359ba438
KingWndr15 <abrahameyaife@gmail.com>
2024-08-06 22:40:36 +0100

    added the vercel.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ['@pinia/nuxt'],
-})
+  modules: ["@pinia/nuxt"],
+  build: {
+    transpile: ["xlsx"],
+  },
+});
